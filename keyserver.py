034c17e3f86a7f2791aa30aea4d863adc742788f
Sam Hoffman <samuelhoffman2@gmail.com>
2018-01-27 23:25:39 -0600

    only load DevelopmentConfig when the environment variable FLASK_DEBUG is set

--- keyserver.py
+++ keyserver.py
@@ -1,6 +1,11 @@
+import os
+
 from keyserv import create_app
 
-app = create_app("DevelopmentConfig")
+if os.environ.get("FLASK_DEBUG"):
+    app = create_app("DevelopmentConfig")
+else:
+    app = create_app("ProductionConfig")
 
 if __name__ == '__main__':
     app.run()
